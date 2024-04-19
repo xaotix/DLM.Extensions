f70915e0a1581f1e883bf1dee1a5fe4d92507edf
Daniel Lins Maciel <dan.8p@hotmail.com>
2024-04-19 11:30:39 -0300

    Ajustes

--- DLM.Extensions/Extensoes_Prompt.cs
+++ DLM.Extensions/Extensoes_Prompt.cs
@@ -3,6 +3,7 @@ using DLM.vars;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,13 +13,13 @@ namespace Conexoes
     {
         public static int StringMaxLenght => 500;
         public static int Decimais => 5;
-        private static string MainPrompt(string titulo, string Valor, bool gravar_carregar, string chave, bool multilinha, int maxlenght)
+        private static object MainPrompt(string titulo, object Valor, bool gravar_carregar, string chave, bool multilinha, int maxlenght, string string_format = null)
         {
             if (maxlenght <= 0)
             {
                 maxlenght = StringMaxLenght;
             }
-            string retorno = null;
+            object retorno = Valor;
             string arq = Cfg.Init.DIR_APPDATA + @"cfguser_prompt.dlm";
             if (chave.Length == 0)
             {
@@ -26,11 +27,11 @@ namespace Conexoes
             }
             if (gravar_carregar)
             {
-                Valor = INI.Get(arq, "Prompt", chave, Valor);
+                Valor = INI.Get(arq, "Prompt", chave, Valor.ToString());
             }
             if (multilinha)
             {
-                var mm = new Janelas.Texto(Valor);
+                var mm = new Janelas.Texto(Valor.ToString());
                 mm.Title = titulo;
                 mm.ShowDialog();
 
@@ -41,14 +42,36 @@ namespace Conexoes
             }
             else
             {
-                var mm = new Digita_Texto(titulo, maxlenght, Valor, titulo);
-                mm.ShowDialog();
-                if (mm.DialogResult.HasValue && mm.DialogResult.Value)
+                if (Valor is double)
                 {
-                    retorno = mm.caixa_texto.Text;
-                    if (gravar_carregar && retorno != "")
+                    var mm = new DLM.WPF.Pompt.PromptDouble(Valor.Double(), titulo, string_format);
+                    mm.Title = titulo;
+
+                    mm.ShowDialog();
+                    if (mm.DialogResult.HasValue && mm.DialogResult.Value)
                     {
-                        INI.Set(arq, "Prompt", chave, retorno);
+                        retorno = mm.MVC.Valor;
+                        if (gravar_carregar && retorno != null)
+                        {
+                            INI.Set(arq, "Prompt", chave, retorno);
+                        }
+                    }
+                }
+                else
+                {
+                    var mm = new Digita_Texto(titulo, maxlenght, Valor, titulo);
+                    mm.ShowDialog();
+                    if (mm.DialogResult.HasValue && mm.DialogResult.Value)
+                    {
+                        retorno = mm.caixa_texto.Text;
+                        if (gravar_carregar && retorno != null)
+                        {
+                            INI.Set(arq, "Prompt", chave, retorno);
+                        }
+                    }
+                    else
+                    {
+                        retorno = null;
                     }
                 }
             }
@@ -67,90 +90,48 @@ namespace Conexoes
             return retorno;
         }
 
-        public static string Prompt(this string valor, bool multi_line, string titulo, int  max_lenght = 0)
+
+        public static string Prompt(this string valor, string titulo = "Digite", int max_lenght = 0, bool multilinha = false, bool salvar_carregar = false, [CallerMemberName] string salvar_carregar_chave = "")
         {
-            return MainPrompt(titulo,valor,false,"",true, max_lenght);
-        }
-        public static string Prompt(this string valor, string titulo = "Digite", int max_lenght = 0)
-        {
-            var status = false;
-            return Prompt(valor, out status, titulo);
-        }
-        public static string Prompt(this string valor, out bool status, string titulo = "Digite")
-        {
-            status = false;
-            var retorno = MainPrompt(titulo, valor, false, "", false, 0);
+            var retorno = MainPrompt(titulo, valor, salvar_carregar, "", multilinha, max_lenght);
             if (retorno == null)
             {
                 return null;
             }
             else
             {
-                status = true;
-                return retorno;
+                return retorno.ToString();
             }
         }
-        public static string Prompt(this string valor, string titulo, string chave, int max_lenght = 0)
+
+        public static int? Prompt(this int valor, string titulo = "Digite")
         {
-            var retorno = MainPrompt(titulo, valor, true, chave, false, max_lenght);
-            return retorno;
+            var retorno = ((double)valor).Prompt(titulo, 0,"N0");
+            if (retorno != null)
+            {
+                return (int)retorno;
+            }
+            return null;
         }
 
-        public static int Prompt(this int valor, string titulo = "Digite")
+
+
+        public static double? Prompt(this double valor, string titulo = "Digite", int decimais = -1, string string_format = null, bool gravar_carregar = false, string chave = "")
         {
-            var status = false;
-            var retorno = valor.Prompt(out status, titulo);
-            return retorno;
-        }
-        public static int Prompt(this int valor, out bool status, string titulo = "Digite")
-        {
-            status = false;
-            var retorno = MainPrompt(titulo, valor.ToString(), false, "", false,15);
+            if (decimais < 0)
+            {
+                decimais = Decimais;
+            }
+            var retorno = MainPrompt(titulo, valor, gravar_carregar, chave, false, 15);
             if (retorno == null)
             {
-                return -1;
+                return null;
             }
             else
             {
-                status = true;
-                return retorno.Int();
-            }
-        }
-
-
-        public static double Prompt(this double valor, string titulo = "Digite", int decimais = 5)
-        {
-            var status = false;
-            var retorno = valor.Prompt(out status, decimais,titulo);
-            return retorno;
-        }
-        public static double Prompt(this double valor, out bool status, int decimais = -1, string titulo = "Digite")
-        {
-            if(decimais<0)
-            {
-                decimais = Decimais;
-            }
-            status = false;
-            var retorno = MainPrompt(titulo, valor.String(decimais), false, "", false, 15);
-            if (retorno == null)
-            {
-                return 0;
-            }
-            if (retorno.Length > 0)
-            {
-                status = true;
                 return retorno.Double(decimais);
             }
-            return 0;
         }
-        public static double Prompt(this double valor, string titulo, string chave,int decimais = -1)
-        {
-            if(decimais<0)
-            {
-                decimais = Decimais;
-            }
-            var retorno = MainPrompt(titulo, valor.ToString(), true, chave, false, 0);
-            return retorno.Double(decimais);
-        }
+
     }
 }
