b8c8e6578b2ca026f4716d41cd811a5aa4ac359c
xaotix <dan.8p@hotmail.com>
2025-12-12 14:03:39 -0300

    Update Extensoes_Alerta.cs

--- DLM.Extensions/Extensoes_Alerta.cs
+++ DLM.Extensions/Extensoes_Alerta.cs
@@ -101,15 +101,17 @@ namespace Conexoes
         {
             new Action(() =>
             {
-                var mm = new VerTextoReport();
-                mm.texto.Text = $"{titulo}\n\n{texto}";
-                mm.Title = $"Erro! {titulo.Esquerda(30)}";
-                mm.Topmost = true;
-                mm.texto.Foreground = System.Windows.Media.Brushes.Red;
-                mm.texto.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
-                mm.texto.TextAlignment = TextAlignment.Left;
-                mm.texto.VerticalAlignment = VerticalAlignment.Stretch;
-                mm.Show();
+                var report = new List<Report> { new Report(titulo, $"{titulo}\n\n{texto}", TipoReport.Critico) };
+                report.Show();
+                //var mm = new VerTextoReport();
+                //mm.texto.Text = $"{titulo}\n\n{texto}";
+                //mm.Title = $"Erro! {titulo.Esquerda(30)}";
+                //mm.Topmost = true;
+                //mm.texto.Foreground = System.Windows.Media.Brushes.Red;
+                //mm.texto.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                //mm.texto.TextAlignment = TextAlignment.Left;
+                //mm.texto.VerticalAlignment = VerticalAlignment.Stretch;
+                //mm.Show();
             }).NovaInstancia();
         }
         public static void JanelaTexto(this string mensagem, string titulo = "", bool top = true, bool pendurar = false)
@@ -129,9 +131,6 @@ namespace Conexoes
             {
                 mm.Show();
             }
-
-
-
         }
         public static void NovaInstancia(this Action chamada)
         {
